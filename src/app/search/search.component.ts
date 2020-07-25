501a3d37837db232ad5ad1f215fc50a122ec7efb
Giulia Tondin <giu.tondin@gmail.com>
2020-07-25 12:51:47 -0300

    Added scss to search section

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -12,22 +12,35 @@ import { Router } from '@angular/router';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
   movies: any[] = [];
   searchFormControl = new FormControl();
   value: any;
-  msg = '';
-  isDone = true;
+  // msg = '';
+  isDone = false;
+
+  logo = {
+    img: './assets/imgs/imdb-logo.png',
+    img_alt: 'Logo IMDB'
+  }
+  input = {
+    placeholder: 'Digite o nome do filme',
+    type: 'text',
+    autocomplete: 'off'
+  }
+  welcome = {
+    img: './assets/imgs/welcome-img.svg',
+    img_alt: 'Imagem de boas vindas'
+  }
 
   constructor(private router: Router, private appService: AppService) { }
 
   ngOnInit() {
     this.searchFormControl.valueChanges
-      .pipe(debounceTime(200))
+      //.pipe(debounceTime(200))
       .subscribe(newValue => {
       this.isDone = false;
       this.value = newValue;
-      this.refresh();
+      // this.refresh();
     }
     );
   }
@@ -41,17 +54,17 @@ export class SearchComponent implements OnInit {
             items.push(data[key]);
           }
         }
-        this.msg = `Mostrando resultados para ${this.value}...`;
+        // this.msg = `Mostrando resultados para ${this.value}...`;
         this.movies = items[0];
         this.isDone = true;
       } else {
         this.isDone = true;
         this.movies.length = null;
-        if(this.value === ''){
-          this.msg = `Por favor, insira o nome de algum filme`;
-        } else {
-          this.msg = `Nenhum filme relacionado foi encontrado`;
-        }
+        // if(this.value === ''){
+        //   this.msg = `Por favor, insira o nome de algum filme`;
+        // } else {
+        //   this.msg = `Nenhum filme relacionado foi encontrado`;
+        // }
       }
     })
   }
